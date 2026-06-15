110d1a0696cb6a8342000b1a385f7931a24a309a
Kshydlo <zikdanbar@gmail.com>
2026-06-15 17:39:12 +0900

    Исправление Д/З (Задача №1)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задача 1
 
+        // Задача 1
         System.out.println("Задача №1");
         int clientOS = 0; // 0 — iOS, 1 — Android
 
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1) {
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
